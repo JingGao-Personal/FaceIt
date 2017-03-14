9fb0f100c91f41e947a1ce364b0fb6fbde38f93b
Jing Gao <jinggao@Jings-MacBook-Pro-2.local>
2017-03-14 17:58:23 +1100

    learned from Stanford University course

---
+++ FaceIt/FaceViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Jing Gao on 28/2/17.
+//  Copyright © 2017 Jing Gao. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController
+{
+    var expression = FacialExpression(eyes: .Closed, eyeBrows: .Relaxed, mouth: .Smirk) {
+        didSet {
+            updateUI()
+        }
+    }
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: #selector(faceView.changeScale(recognizer:))
+            ))
+            let happierSwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(FaceViewController.increaseHappiness)
+            )
+            happierSwipeGestureRecognizer.direction = .up
+            faceView.addGestureRecognizer(happierSwipeGestureRecognizer)
+            
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: #selector(faceView.changeScale(recognizer:))
+            ))
+            let sadderSwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(FaceViewController.decreaseHappiness)
+            )
+            sadderSwipeGestureRecognizer.direction = .down
+            faceView.addGestureRecognizer(sadderSwipeGestureRecognizer)
+            
+            updateUI()
+        }
+    }
+    @IBAction func toggleEyes(_ sender: UITapGestureRecognizer) {
+        if sender.state == .ended {
+            switch expression.eyes {
+            case .Open: expression.eyes = .Closed
+            case .Closed: expression.eyes = .Open
+            case .Squinting: break
+            }
+        }
+        
+    }
+    
+    func increaseHappiness()
+    {
+        expression.mouth = expression.mouth.happierMouth()
+    }
+    
+    func decreaseHappiness()
+    {
+        expression.mouth = expression.mouth.sadderMouth()
+    }
+    
+    private var mouthCurvatures = [FacialExpression.Mouth.Frown:-1.0,.Grin:0.5,.Smile:1.0,.Smirk:-0.5,.Neutral:0.0 ]
+    private var eyeBrowTilts = [FacialExpression.EyeBrows.Relaxed:0.5,.Furrowed:-0.5,.Normal:0.0]
+    
+    private func updateUI() {
+        if faceView != nil {
+            switch expression.eyes {
+            case .Open: faceView.eyesOpen = true
+            case .Closed: faceView.eyesOpen = false
+            case .Squinting: faceView.eyesOpen = false
+            }
+            faceView.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
+            faceView.eyeBrowTilt = eyeBrowTilts[expression.eyeBrows] ?? 0.0
+        }
+        
+    }
+}
+
---
+++ FaceIt/FacialExpression.swift
@@ -0,0 +1,51 @@
+//
+//  FacialExpression.swift
+//  FaceIt
+//
+//  Created by Jing Gao on 4/3/17.
+//  Copyright © 2017 Jing Gao. All rights reserved.
+//
+
+import Foundation
+
+struct FacialExpression
+{
+    enum Eyes: Int {
+        case Open
+        case Closed
+        case Squinting
+    }
+    
+    enum EyeBrows: Int {
+        case Relaxed
+        case Normal
+        case Furrowed
+        
+        func moreRelaxedBrow() -> EyeBrows {
+            return EyeBrows(rawValue: rawValue - 1) ?? .Relaxed
+        }
+        func moreFurrowedBrow() -> EyeBrows {
+            return EyeBrows(rawValue: rawValue + 1) ?? .Furrowed
+        }
+    }
+    
+    enum Mouth: Int {
+        case Frown
+        case Smirk
+        case Neutral
+        case Grin
+        case Smile
+        
+        func sadderMouth() -> Mouth {
+            return Mouth(rawValue: rawValue - 1) ?? .Frown
+        }
+        func happierMouth() -> Mouth {
+            return Mouth(rawValue: rawValue + 1) ?? .Smile
+        }
+        
+    }
+    
+    var eyes: Eyes
+    var eyeBrows: EyeBrows
+    var mouth: Mouth
+}
